fde097c411a5a77c3105adf0e1660b7058169781
elsaad92 <99730108+elsaad92@users.noreply.github.com>
2023-01-19 09:53:27 +0300

    layout/base,partial/header/footer,radio/index/register/login

---
+++ radio/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name='radio'
+
+urlpatterns = [
+    path('index/', views.home_view, name='index'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+]
---
+++ radio/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+def home_view(request):
+        return render(request, 'radio/index.html')
+
+def register_view(request):
+        return render(request, 'radio/register.html')
+   
+
+@login_required
+def login_view(request):
+        if request.method == 'POST':
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(request, email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
+            else:
+                # invalid login
+                pass
+        return render(request, 'login.html')
